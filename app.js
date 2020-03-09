c7a172facf73a6d7e387a4d6928f12ebced11a19
Jake O'Rielly <jake.orielly@gmail.com>
2020-03-08 23:01:20 -0400

    Added attack and damage functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const attributesMaster = ["name","strength","dexterity","constitution","intelligence","wisdom","charisma"];
-
-function Colonist(attributes){ 
+let creatures = [];
+function Creature(attributes){ 
     this.name = attributes.name;
     this.strength = attributes.strength;
     this.dexterity = attributes.dexterity;
@@ -8,6 +8,26 @@ function Colonist(attributes){
     this.intelligence = attributes.intelligence;
     this.wisdom = attributes.wisdom;
     this.charisma = attributes.charisma;
+
+    this.maxHP = function() {
+        return this.constitution * 4;
+    }
+
+    this.hp = this.maxHP();
+
+    this.takeDamage = function(damage) {
+        this.hp -= damage;
+        if (this.hp <= 0)
+            this.die();
+    }
+
+    this.die = function() {
+        console.log(this.name + ' is dead.')
+    }
+
+    this.attack = function(target) {
+        target.takeDamage(this.strength);
+    }
  } 
 
 $('#create-colonist').click(
@@ -18,7 +38,8 @@ function createColonist() {
     let attributes = {};
     for (let i of attributesMaster)
         attributes[i] = $('#' + i + '-input').val();
-    let newColonist = new Colonist(attributes);
+    let newColonist = new Creature(attributes);
+    colonists.push(newColonist);
     $('#colonist-list').append('<li>' + newColonist.name + '</li>');
     console.log(newColonist)
 }
