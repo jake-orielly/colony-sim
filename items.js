b0f2edb5bdfbd8df631329dbf6452d0bafd778f7
Jake O'Rielly <jake.orielly@gmail.com>
2020-03-22 09:36:18 -0400

    Moved weapons to their own file

--- items.js
+++ items.js
@@ -4,36 +4,3 @@ class Item {
         this.value = attributes.value;
     }
 }
-
-class Weapon extends Item {
-    constructor(attributes) {
-        super(attributes);
-        this.damage = attributes.damage;
-        this.damageType = attributes.damageType;
-        this.range = attributes.range;
-    }
-}
-
-class Shortsword extends Weapon {
-    constructor() {
-        super({name:'Shortsword',damage:[1,6],damageType:'slashing',range:1,value:10})
-    }
-}
-
-class Greatsword extends Weapon {
-    constructor() {
-        super({name:'Greatsword',damage:[2,12],damageType:'slashing',range:1,value:10})
-    }
-}
-
-class Shortbow extends Weapon {
-    constructor() {
-        super({name:'Shortbow',damage:[1,6],damageType:'piercing',range:15,value:10})
-    }
-}
-
-class HeavyCrossbow extends Weapon {
-    constructor() {
-        super({name:'Heavy Crossbow',damage:[1,10],damageType:'piercing',range:20,value:10})
-    }
-}
