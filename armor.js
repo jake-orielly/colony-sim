72b37a3e29fba1942e268b2ebafca51e334479d1
Jake O'Rielly <jake.orielly@gmail.com>
2020-03-21 21:47:46 -0400

    Added armor

---
+++ armor.js
@@ -0,0 +1,36 @@
+class Armor extends Item {
+    constructor(attributes) {
+        super(attributes);
+        this.armorBonus = attributes.armorBonus;
+    }
+}
+
+class Breastplate extends Armor {
+    constructor() {
+        super({name:'Breastplate',armorBonus:7,value:10})
+    }
+}
+
+class Platelegs extends Armor {
+    constructor() {
+        super({name:'Platelegs',armorBonus:5,value:10})
+    }
+}
+
+class Greathelm extends Armor {
+    constructor() {
+        super({name:'Greathelm',armorBonus:3,value:10})
+    }
+}
+
+class Gauntlets extends Armor {
+    constructor() {
+        super({name:'Gauntlets',armorBonus:1,value:10})
+    }
+}
+
+class Boots extends Armor {
+    constructor() {
+        super({name:'Boots',armorBonus:2,value:10})
+    }
+}
