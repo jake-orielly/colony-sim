0eb190ce1ad00872b33f640094edda3d6205a4ea
Jake O'Rielly <jake.orielly@gmail.com>
2020-07-20 16:44:17 -0400

    Showing equipment in gui

--- gui.js
+++ gui.js
@@ -5,7 +5,6 @@ function showDetails(selected) {
     $("#info-div").append("<p class='clickable' onclick='toggleDisplay(\"inventory\")'></p>");
     $("#info-div p:last").append("<span class='title'>Inventory</span><span id='inventory-sign'>+</span>");
     $("#info-div").append("<div class='detail-sub' id='inventory-detail'></div>");
-    detailCategories.inventory = false;
     for (let i in selected.inventory)
         $("#inventory-detail").append("<p>" + prettyPrint(i) + ": " + selected.inventory[i].amount + "</p>");
     if (!Object.keys(selected.inventory).length)
@@ -16,9 +15,16 @@ function showDetails(selected) {
         $("#info-div").append("<div class='detail-sub' id='attr-detail'></div>");
         for (let i in selected.attr)
             $("#attr-detail").append("<p>" + prettyPrint(i) + ": " + selected.attr[i] + "</p>");
-        detailCategories.attr = false;
     }
-    $(".detail-sub").hide()
+    if (selected.equipment != undefined){
+        $("#info-div").append("<p class='clickable' onclick='toggleDisplay(\"equipment\")'></p>");
+        $("#info-div p:last").append("<span class='title'>Equipment</span><span id='equipment-sign'>+</span>")
+        $("#info-div").append("<div class='detail-sub' id='equipment-detail'></div>");
+        for (let i in selected.equipment)
+            $("#equipment-detail").append("<p>" + prettyPrint(selected.equipment[i].name) + "</p>");
+    }
+    for (let i of ["inventory","attr","equipment"])
+        detailContract(i)
 }
 
 function toggleDisplay(val) {
@@ -29,7 +35,8 @@ function toggleDisplay(val) {
 }
 
 function detailExpand(val) {
-    $(".detail-sub").hide()
+    for (let i of ["inventory","attr","equipment"])
+        detailContract(i)
     $("#" + val + "-detail").show();
     $("#" + val + "-sign").html('-');
     detailCategories[val] = true;
